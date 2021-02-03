e398321fa8ab8c3eab6111b2a6fd131d414eaabb
Groveet Yolandi <groveet2@gmail.com>
2021-02-03 23:38:23 +0300

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,21 @@
 <template>
+  <app-navbar></app-navbar>
   <div id="nav">
     <router-link to="/">Home</router-link> |
     <router-link to="/about">About</router-link>
   </div>
   <router-view/>
 </template>
+
+<script>
+import AppNavbar from '@/components/AppNavbar'
+
+export default {
+  components: {
+    AppNavbar
+  }
+}
+</script>
 
 <style lang="scss">
 #app {
